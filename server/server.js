105f38ceb9c57cf2852ce5cdb44b5bdb7eaa27b5
Popoy Vargas <ipopoyMM@popoy-mm.local>
2018-05-17 16:54:33 +0800

    initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require( 'express' );
+const path = require( 'path' );
+const publicPath = path.join( __dirname, '../public' );
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use( express.static( publicPath ) );
+
+app.listen( port, () => console.log( `Started on port ${ port }` ) );
